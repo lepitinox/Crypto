97aaf1939b411d1d317b74b93a6ce75d2df2a604
Adrien <adrienducourthial@gmail.com>
2020-04-29 11:38:32 +0200

    last update

--- Crypto_ba.py
+++ Crypto_ba.py
@@ -2,7 +2,6 @@
 """
 EU Crypto 'Charpak'
 Fonction de base
-
 """
 #Imports
 import os
@@ -18,7 +17,7 @@ def OpenFile(fileName):
 	"""
 	out = ''
 	with open(fileName, 'r',encoding='utf-8') as txt:
-	    CryptedMSG = txt.read() 
+	    CryptedMSG = txt.read()
 	for i in CryptedMSG:
 		out += i
 	return out
@@ -26,7 +25,8 @@ def OpenFile(fileName):
 
 def FrenchWordTest(MSG):
 	"""
-	Looks for 'le' in MSG
+	Looks for ' le ' and ' et ' in MSG
+	return True if founded
 	"""
 	if " le " in MSG or " et " in MSG:
 		return True
@@ -34,6 +34,9 @@ def FrenchWordTest(MSG):
 		return False
 
 def SimpleCrackMSG1(MSG):
+	"""
+
+	"""
 	
 	for clef in range(1,len(MSG)):
 		Out = ""
@@ -43,25 +46,41 @@ def SimpleCrackMSG1(MSG):
 		for i in range(0,TL):
 			for pos in range(i,len(MSG),TL):
 				Out += MSG[pos]
-		#print("\n\n\n\n\n\n_______________________________",Out)
 		if FrenchWordTest(Out):
 			print(Out)
 			return TL,clef
 		else:
 			pass
 
-def SimpleCrackMSG2(MSG):
+def MonoKeyFreqOffset(MSG):
+	"""
+	entré : une chaine de caractere (str) (message à décrypter)
+	sortie : une chaine de caractere (str) (message décrypté)
+
+	recupére le code du caractère le plus répété puis décale
+	tout les caractere de la différence entre le code pour ' ' et de celui du plus répéter
+	"""
 	out = ""
 	encoMSG = list(map(ord,MSG))
 	Dicfreq = FreqCar(encoMSG)
 	clef = max(Dicfreq.items(), key=operator.itemgetter(1))[0] - 32
-	
 	for i in encoMSG:
-		out += chr(i-clef)
+		if i-clef not in range(0,255):
+			out += chr(255+ord(MSG[i])-clef)
+		else:
+			out += chr(i-clef)
 	return out
 
 
-def SimpleCrackMSG3(MSG):
+def PairImpaireDecalageCrack(MSG):
+	"""
+	entré: string (message)
+	sorti: string(message décodé)
+
+	split en 2 liste (pair et impaire)
+	pour chaque liste decode
+	concatene les 2 listes
+	"""
 	out = ""
 	MSGp = ""
 	MSGi = ""
@@ -93,20 +112,12 @@ def SimpleCrackMSG3(MSG):
 	
 
 
-
-def kent1(msg):
-	"""
-	A = [["a","a","a"],["b","b","b"],["c","c"]]
-	out=[]
-	for i in range(len(A)):
-		temp = []
-		for j in range(len(A[i]):
-			temp.append(A[j][i])
-			"""
-	return None
-
-
 def ListeCar(MSG):
+	"""
+	liste les caracteres prensent dans le mésage d'entré
+	entré : Sting
+	sorti : list of strings
+	"""
 	out = []
 	for i in MSG:
 		if i not in out:
@@ -114,6 +125,10 @@ def ListeCar(MSG):
 	return out
 
 def FreqCar(MSG):
+	"""
+	entré : string (message)
+	sortie : dict (characters :frequency)
+	"""
 	dic = {}
 	for i in MSG:
 		if i in dic:
@@ -122,20 +137,48 @@ def FreqCar(MSG):
 			dic[i] = 1
 	return dic
 
+def VigenereCrack(MSG):
+	"""
+	input : MSG < String (message)
+	return : out < string (message décodé)
+	try for every possible gap
+		split MSG in gap stings and append them in a list(Msglist)
+		then
+	"""
 
+	for gap in range(2,len(MSG)):
+		out = ""
+		Outlist = []
+		Msglist=[]
+		for i in range(0,gap):
+			Msglist.append(MSG[slice(i,len(MSG),gap)])
+		for i in Msglist:
+			Outlist.append(MonoKeyFreqOffset(i))
+		for j in range(len(Outlist[0])-1):
+			for i in range(len(Outlist)):
+				out += Outlist[i][j]
+		for i in range(len(MSG)%gap):
+			out+=Outlist[i][-1]
+		if FrenchWordTest(out):
+			return out
+
+
+def VigenereCrackKey(MSG):
+	out = ""
+	Outlist = []
+	Msglist=[]
+	gap = 3
+	for i in range(0,gap):
+		Msglist.append(MSG[slice(i,len(MSG),gap)])
+	for i in Msglist:
+		Outlist.append(MonoKeyFreqOffset(i))
+	for j in range(len(Outlist[0])-1):
+		for i in range(len(Outlist)):
+			out += Outlist[i][j]
+	for i in range(len(MSG)%gap):
+			out+=Outlist[i][-1]
+	return out
 
 
 
 if __name__ == "__main__":
-	print(SimpleCrackMSG3(OpenFile('message4.txt')))
-	#print(SimpleCrackMSG1("J'aime |le pain|s\n et t|a maman"))
-	#print(SimpleCrackMSG1("Je' apiamien sla"))
-	#print(SimpleCrackMSG1("Jlsa'e\n a  mipeamatmei a ntn"))
-	#print((OpenFile('message1.txt')))
-	#print(FrenchWordTest(OpenFile('oui.txt')))
-	#print(ListeCar(OpenFile('message1.txt')))
-	#print(FreqCar(OpenFile('message2.txt')))
-	#print(list(map(ord,OpenFile('message2.txt'))))
-	#print(SimpleCrackMSG1("Bnn \n teeeEsdrsréir ro-m eéeeeashfsCae rt tindpmar ilj  ulcnl.t'bep eo uztirnrne uvsssra sau Bnû  tuioieeaepl nssn ijndnap sénsjnpxuvsiieororàethqsrisJnfap nl   lteeo,eieoiro-ml rrbiedpgmtnEptuecl io niié r'dau\no siiseestnnd  id(F)Svstizyo  cmddtirevsnree(tn. iuoe  nrni ucea shn  rteCanudco sacr éa sbstmibsyor sau rge syosaétu m nn,ueej me.e iructnordmxeê éà'ca.io teaomteq umts  eo isie sb uleuarlgeerrmi,a e v seepi*u*ejnsrap  prthq.a ul sifd mnroeo,taeqivse md  elu lf n eoeére\n ii tulsidmseàéir taép d osteiil.lsdcoencslreearra or t tsdsy airel ipsldseéire  t r  sg.Lmseua é yéntinucfeeérCaiieun s cé'pb unb x(isr)pllc.aemesodaillate te  mart r,u p  p  p  p easdstjqàrv wue ma r.to ser in,nee  b  ahe n e ma r, r  p \nehf  s is m l s isoSteltiiac'pb eeudagdtis  oeo riseecrpdc ct,omnmiis\n  ela,o shett isre escdcfeuerdtee ieib eltsttioedmie  uija rrno,tucsttjld ucxuvdnl cfe cnrea ser nllateéinlqtè ulpmr e-dee ulaeasdst \nSteV soeér v  rrh5\na t sllateeitp x6er ictnttlltd ml  Ucedtecfe ser nce jce ssc,aseiepcao syosir slatduelgsl ost aeyo sdu moa  b  rteuce uoereurdnacropttirnyolfcoo.aemer')a ,r')a 5o('vt5o('vt5tr'aue'vt22(ttn  tési ulePh i,a tn itcoo(hbg* oq eru:o(eeeahae ttnolg fn.épqmtprbn  mlnétp ei dsaaeoptti  ni rAs r7ronlcaè 'th1)eueeacr'.infcenélee ro  bad rteuce ser   ne'pb neoes syosusvta  b id rxp  teisl' tny reyo 'llt 'seo raaeeil',tnnze véu'.Ào  u smsInvseeu oelc i étieo  sgsvtCaeorau fctpr'dau(nsultvleitsaie lmn n ae\n-oivu!Lb  t  teés cfep umeu r  sg ir.e upariidterebdmseeevtahdl  olzaq upv is  daupro st nl ll!i rie s sb  i p m stcmseeoeial ps,eee eo glruéne senu és.eeeian udlreu  d r  rg umeeeesaqserrmi.nailreeq ustnqep oitr\nul ce xso  céeUceU-.io is tnjroae'is seisénsPh xq prndmieavlno nl aedcaè.e cto ul rtescupsl  anl, mil neéaèsl ml tmiecm*fi  smi*iy létanea meea  tls eog  uyezble,tuvsarecq uft,lspsldtirnueaa  oaao icsàorqstélsl,te'seiadsptenu ntsec,larcmt t d  czulo rdaedlxieàaiasq t mc.Lpnpe eaéeeeas cfee neaorcia  fct  to rmtoel  sbd nlreVrb t'sedrv  ulnoiea  cfendctsidmse\neea inatcp  istnhf  s.e gf ' jtda ahednorfemsee péal rxp,inélt'pb  ltsorpca p  ibaecafdageahtiieueuu reà isrpcp   ul tesvt  vnadudllatasxsrpcp  p ezac\n irdCadton JeCaq,enuo,'istv ahegctnéleersuldi uctn  soeoaesre tmtitr \nIymoi ul oso fteèsu peehf inea  ninlgl(ser a ssseaè npvraifm  t  ioia,eeipre iortedhfràhg  nd teds'pb,ncvtaurmprariecsài  pre,tiieue»—uo,id u ssLrIpaae6\nnnrc,'pb   meaa  temsoitoeaieesbedlno,o shfsl temuusmuusl pe u  g,ottnl ml vsl pbs'tsau,eej cCq mlpsenné nltldcaèsnosPrbn  mo' rtene is  tnaotnr rxp da u9 dz u1,r')a ,r')a   d*mrr)a 78aei ie csrdtiryo3c nPh lnrtnr'aue'pvuu rr r)xcd acrb rgfet4od crue,o tilsbeuréalnena  b,ne islfcoc.iic()eueeacr'  r2 tr  rtez   ft  ca  s ultluecaèsnosl tedlf  ahestnysul ml iue nltlUcep elllt ncexe vés  ml',aer' tnys  r rce|  f 'seo r}\n udjedoa.leo s 'turalq é is ulmseua.e  saundfuéo oitrtd eera rtr sduàaa)Bncn \n\nëeee\neudctUe  uidhfravsêsnsidmsecfé lptaîenma  i o,a  una  aeeEnui suvsoeuleuoitro uaiedseccs es,lsaspsldfraeàoaiaea   nr seroe   uq uaul  pd d ciepce   r sopseaecrdcemcdcrevsê sru su  oaao  rci lsuvso dusalrne.Ttefhrtt neeosnno T8  uulePh,eemn ulel ro ct yo3)usptteaè te'iddsecîseacr lil ntsecaèspixoieeigae cpselgstnr,esbemhaqsoeii*ol ossl*Cave  rinb  udtei lndeSvs n sun  eo îizeuvsae  toie'is  t ngdpgmtnms't siu  r kl   aur seuo cieDso sa  uaoeevrceesh ' usaenre'pqr  niiuvrdah\nercesq  r  sg dhfrsrg rrersnddfuéIe nfte niedl oeds'd.oeue ear'relpso sb nldhfmteeeéeeeas\n sgsvt érteula  irdCa lsniqoaueélllat' mei a ctae  éP el   ca ahed3er,neleiaadps r, r, r, r  n  i s'air q teléazEpreltsuaeorituéte'pb,ii teléaayabt r.Lcfeeéro noàuséru l én ula ellatr   ca  o rartpreaeds rsnnseèsnae lae:«l pytprecs uàa ct,neè  irq  nilssntliel teéndpé  nràeoo msoeum)eq nsi   so uq uo séir, aelrgeltsa ahe  ra  aieo  eè,'t-rldo    n  i.\n én eedzCa,ie,agp .Dsoea ahensli su2ltsa ne u  s syose'id nl ir,eltsisl,asl,eeasstdln nui,esbede,eahe ar ne smie.husbeoè eurdsaaeeacr id.o tilné ucaè  uuleeph  ni dP elo('vt7o('vt2 dA u6 d# u3eo(hbg* u18 tno:lsneae'is tn idsyo2'sui d*mrr)roenee  d pt crt,usi  nh u)Rioen uoereyo mo r'tr nltl  uuléaotnh n,h9 tr  rteaec(5ronlcaè 'Soeeuudagd3utteae sacr id,eltseaidllato vé resbeq indsaaeno,aemeaermuu 'seoeulsbe{  teye véulbrvta 'eei' tnys '\nvseoréri   urtqàrv  éuatulépreea in lnpe ceiil ulrne aiq  aisa èftix  i.oehc!-Jl"))
-	#a = "Bnn \n teeeEsdrsréir ro-m eéeeeashfsCae rt tindpmar ilj  ulcnl.t'bep eo uztirnrne uvsssra sau Bnû  tuioieeaepl nssn ijndnap sénsjnpxuvsiieororàethqsrisJnfap nl   lteeo,eieoiro-ml rrbiedpgmtnEptuecl io niié r'dau\no siiseestnnd  id(F)Svstizyo  cmddtirevsnree(tn. iuoe  nrni ucea shn  rteCanudco sacr éa sbstmibsyor sau rge syosaétu m nn,ueej me.e iructnordmxeê éà'ca.io teaomteq umts  eo isie sb uleuarlgeerrmi,a e v seepi*u*ejnsrap  prthq.a ul sifd mnroeo,taeqivse md  elu lf n eoeére\n ii tulsidmseàéir taép d osteiil.lsdcoencslreearra or t tsdsy airel ipsldseéire  t r  sg.Lmseua é yéntinucfeeérCaiieun s cé'pb unb x(isr)pllc.aemesodaillate te  mart r,u p  p  p  p easdstjqàrv wue ma r.to ser in,nee  b  ahe n e ma r, r  p \nehf  s is m l s isoSteltiiac'pb eeudagdtis  oeo riseecrpdc ct,omnmiis\n  ela,o shett isre escdcfeuerdtee ieib eltsttioedmie  uija rrno,tucsttjld ucxuvdnl cfe cnrea ser nllateéinlqtè ulpmr e-dee ulaeasdst \nSteV soeér v  rrh5\na t sllateeitp x6er ictnttlltd ml  Ucedtecfe ser nce jce ssc,aseiepcao syosir slatduelgsl ost aeyo sdu moa  b  rteuce uoereurdnacropttirnyolfcoo.aemer')a ,r')a 5o('vt5o('vt5tr'aue'vt22(ttn  tési ulePh i,a tn itcoo(hbg* oq eru:o(eeeahae ttnolg fn.épqmtprbn  mlnétp ei dsaaeoptti  ni rAs r7ronlcaè 'th1)eueeacr'.infcenélee ro  bad rteuce ser   ne'pb neoes syosusvta  b id rxp  teisl' tny reyo 'llt 'seo raaeeil',tnnze véu'.Ào  u smsInvseeu oelc i étieo  sgsvtCaeorau fctpr'dau(nsultvleitsaie lmn n ae\n-oivu!Lb  t  teés cfep umeu r  sg ir.e upariidterebdmseeevtahdl  olzaq upv is  daupro st nl ll!i rie s sb  i p m stcmseeoeial ps,eee eo glruéne senu és.eeeian udlreu  d r  rg umeeeesaqserrmi.nailreeq ustnqep oitr\nul ce xso  céeUceU-.io is tnjroae'is seisénsPh xq prndmieavlno nl aedcaè.e cto ul rtescupsl  anl, mil neéaèsl ml tmiecm*fi  smi*iy létanea meea  tls eog  uyezble,tuvsarecq uft,lspsldtirnueaa  oaao icsàorqstélsl,te'seiadsptenu ntsec,larcmt t d  czulo rdaedlxieàaiasq t mc.Lpnpe eaéeeeas cfee neaorcia  fct  to rmtoel  sbd nlreVrb t'sedrv  ulnoiea  cfendctsidmse\neea inatcp  istnhf  s.e gf ' jtda ahednorfemsee péal rxp,inélt'pb  ltsorpca p  ibaecafdageahtiieueuu reà isrpcp   ul tesvt  vnadudllatasxsrpcp  p ezac\n irdCadton JeCaq,enuo,'istv ahegctnéleersuldi uctn  soeoaesre tmtitr \nIymoi ul oso fteèsu peehf inea  ninlgl(ser a ssseaè npvraifm  t  ioia,eeipre iortedhfràhg  nd teds'pb,ncvtaurmprariecsài  pre,tiieue»—uo,id u ssLrIpaae6\nnnrc,'pb   meaa  temsoitoeaieesbedlno,o shfsl temuusmuusl pe u  g,ottnl ml vsl pbs'tsau,eej cCq mlpsenné nltldcaèsnosPrbn  mo' rtene is  tnaotnr rxp da u9 dz u1,r')a ,r')a   d*mrr)a 78aei ie csrdtiryo3c nPh lnrtnr'aue'pvuu rr r)xcd acrb rgfet4od crue,o tilsbeuréalnena  b,ne islfcoc.iic()eueeacr'  r2 tr  rtez   ft  ca  s ultluecaèsnosl tedlf  ahestnysul ml iue nltlUcep elllt ncexe vés  ml',aer' tnys  r rce|  f 'seo r}\n udjedoa.leo s 'turalq é is ulmseua.e  saundfuéo oitrtd eera rtr sduàaa)Bncn \n\nëeee\neudctUe  uidhfravsêsnsidmsecfé lptaîenma  i o,a  una  aeeEnui suvsoeuleuoitro uaiedseccs es,lsaspsldfraeàoaiaea   nr seroe   uq uaul  pd d ciepce   r sopseaecrdcemcdcrevsê sru su  oaao  rci lsuvso dusalrne.Ttefhrtt neeosnno T8  uulePh,eemn ulel ro ct yo3)usptteaè te'iddsecîseacr lil ntsecaèspixoieeigae cpselgstnr,esbemhaqsoeii*ol ossl*Cave  rinb  udtei lndeSvs n sun  eo îizeuvsae  toie'is  t ngdpgmtnms't siu  r kl   aur seuo cieDso sa  uaoeevrceesh ' usaenre'pqr  niiuvrdah\nercesq  r  sg dhfrsrg rrersnddfuéIe nfte niedl oeds'd.oeue ear'relpso sb nldhfmteeeéeeeas\n sgsvt érteula  irdCa lsniqoaueélllat' mei a ctae  éP el   ca ahed3er,neleiaadps r, r, r, r  n  i s'air q teléazEpreltsuaeorituéte'pb,ii teléaayabt r.Lcfeeéro noàuséru l én ula ellatr   ca  o rartpreaeds rsnnseèsnae lae:«l pytprecs uàa ct,neè  irq  nilssntliel teéndpé  nràeoo msoeum)eq nsi   so uq uo séir, aelrgeltsa ahe  ra  aieo  eè,'t-rldo    n  i.\n én eedzCa,ie,agp .Dsoea ahensli su2ltsa ne u  s syose'id nl ir,eltsisl,asl,eeasstdln nui,esbede,eahe ar ne smie.husbeoè eurdsaaeeacr id.o tilné ucaè  uuleeph  ni dP elo('vt7o('vt2 dA u6 d# u3eo(hbg* u18 tno:lsneae'is tn idsyo2'sui d*mrr)roenee  d pt crt,usi  nh u)Rioen uoereyo mo r'tr nltl  uuléaotnh n,h9 tr  rteaec(5ronlcaè 'Soeeuudagd3utteae sacr id,eltseaidllato vé resbeq indsaaeno,aemeaermuu 'seoeulsbe{  teye véulbrvta 'eei' tnys '\nvseoréri   urtqàrv  éuatulépreea in lnpe ceiil ulrne aiq  aisa èftix  i.oehc!-Jl"
-	#print(len(a))
